0b199e7fbd185143b97cf493a33179bcb068199f
Mevlida Salendrkovic <mevlida.salendrkovic@gmail.com>
2024-05-03 16:56:13 +0200

    drawable object

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,19 +1,15 @@
-class MovableObject {
-    x = 120;
-    y = 100;
-    img;
-    height = 150;
-    width = 100;
-    imageCache={};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
+   
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    energy = 100;
+    lastHit = 0;
 
     applyGravity() {
         setInterval(() => {
-            if(this.isAboveGround()){
+            if(this.isAboveGround() || this.speedY > 0) {
                 this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
@@ -23,30 +19,59 @@ class MovableObject {
     isAboveGround() {
        return this.y < 100;
     }
-    loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
-    }
-// arr- array
-    loadImages(arr){
-        arr.forEach((path) => {
-        let img= new Image();
-        img.src = path;
-        this.imageCache[path] = img;
-        });
-    }
-    moveRight(){
+    
 
+    drawFrame(ctx) {
+        if( this instanceof Character || this instanceof Chicken) {
+        ctx.beginPath();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+}
+
+isColliding (mo) {
+    return  this.x + this.width >= mo.x && 
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height;
+    }
+
+hit() {
+    this.energy -= 5;
+    if(this.energy < 0) {
+        this.energy = 0;
+    } else{
+        this.lastHit = new Date().getTime();
+    }
+}
+
+isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit;
+    timepassed = timepassed / 1000;
+    return timepassed < 0.5;
+}
+
+isDead() {
+    return this.energy == 0;
+}
+// arr- array
+   
+    moveRight(){
+        this.x += this.speed;
     }
     moveLeft(){
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
+    }
+
+    jump(){
+        this.speedY = 25;
     }
 
     playAnimation(images) {
          //Walk animation
-         let i = this.currentImage % this.IMAGES_WALKING.length;
+         let i = this.currentImage % images.length;
          let path = images[i];
          this.img = this.imageCache[path];
          this.currentImage++; 
--- models/world.class.js
+++ models/world.class.js
@@ -13,10 +13,21 @@ class World{
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
+        this.checkCollisions();
     }
     
     setWorld(){
         this.character.world = this;
+    }
+
+    checkCollisions(){
+        setInterval(() => {
+            this.level.enemies.forEach((enemy) => {
+                if(this.character.isColliding(enemy)){
+                    this.character.hit();
+                }
+            })
+        }, 200)
     }
 
 
@@ -46,7 +57,10 @@ class World{
         if(mo.otherDirection){
             this.flipImage(mo);
         }
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+       mo.draw(this.ctx);
+       mo.drawFrame(this.ctx);
+        
+        
         if(mo.otherDirection){
             this.flipImageBack(mo);
         }
